70f74c38cea61b69c86814e21e8ca2b6e7d8f0f4
4015e4 <4015e4@china.ece.uprm.edu>
2017-02-08 19:12:15 -0400

    The JFrame now has a Panel added to it with some borders

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,11 +3,16 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Gabriel Colon");
-		
-		
+
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 400);
 		myFrame.setSize(200, 200);
+		
+		MyPanelClass myPanel = new MyPanelClass();
+		myFrame.getContentPane().add(myPanel);
+		
 		myFrame.setVisible(true);
+
+		
 	}
 }
